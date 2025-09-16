eaef6e29484ab929a52415552a46d6829f8b2c55
Gustavo Braga <agbram2005@gmail.com>
2025-09-16 15:13:05 -0300

    get

--- src/routes/rule.js
+++ src/routes/rule.js
@@ -4,5 +4,6 @@ import { RuleController } from "../controllers/rule.js";
 const router = Router();
 
 router.post("/", RuleController.store);
+router.get("/", RuleController.index);
 
 export default router;
