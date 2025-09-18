9bb28d08b2f3efc652a976de63c47e946b16aef2
DasFiel <jeanhsantosfiel07@gmail.com>
2025-09-18 13:37:09 -0300

    Delete carts

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -9,4 +9,8 @@ router.post("/", CartController.store);
 // Listar todos os carrinhos GET /carts
 router.get("/", CartController.index);
 
+
+// Obter um carrinho específico GET /carts/:id
+Router.get("/:id", CartController.show);
+
 export default router;
